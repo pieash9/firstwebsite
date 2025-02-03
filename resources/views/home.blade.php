80f151161769d1bfbf875159d766197780e28a7a
Md. Pieash Ahmed <pieash9@gmail.com>
2025-02-03 11:47:07 +0600

    init

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    @vite("resources/css/app.css")
+    @vite("resources/js/app.js")
+</head>
+<body>
+    <h1 class="text-7xl text-red-500">Home</h1>
+</body>
+</html>
